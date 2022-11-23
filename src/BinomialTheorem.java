c9ec1c7e4d3ea28101dcd3d46f579885d6a8a704
Leting Cai <letingc@nycstudents.net>
2022-11-23 12:24:48 -0500

    Second

--- src/BinomialTheorem.java
+++ src/BinomialTheorem.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class BinomialTheorem {
+public class BinomialTheorem {
+    //Instance Variables
+    private int n, r;
+    private String str;
+
+    //Constructors
+    public BinomialTheorem(int totalNumberOfObjects, int numberOfObjectsSelected){
+        n = totalNumberOfObjects;
+        r = numberOfObjectsSelected;
+    }
+    public BinomialTheorem(){
+        int n = 1;
+        int r = 1;
+    }
+
+    //Methods
+    public int nFactorial(){
+        int ans=1;
+        for (int nF=n; nF>0; nF--){
+            ans*=nF;
+        }
+        return ans;
+    }
+
+    public int rFactorial(){
+        int ans=1;
+        for (int rF=r; rF>0; rF--){
+            ans*=rF;
+        }
+        return ans;
+    }
+
+    public int nMinusRFactorial(){
+        int ans=1;
+        for (int nrF=(n-r); nrF>0; nrF--){
+            ans*=nrF;
+        }
+        return ans;
+    }
+
+    public int combination(){
+        return permutation()/rFactorial();
+    }
+
+    public int permutation(){
+        return nFactorial()/nMinusRFactorial();
+    }
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static double Twc(double Ta, double V){
+        return Math.round(35.74+(0.6215*Ta)-(Math.pow(V,0.16)*35.75)+(0.4275*Ta*Math.pow(V, 0.16)));
+    }
+    public static void main(String[] args){
+        for (int v = 5; v<=60; v+=5){
+            for(int Ta = 40; Ta>=-45; Ta-=5){
+                System.out.print(Twc(Ta,v)+" ");
+            }
+            System.out.println();
+        }
+    }
 }
