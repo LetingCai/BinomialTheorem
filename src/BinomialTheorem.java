13340457908e74089b43208322da240e69569492
Leting Cai <letingc@nycstudents.net>
2022-11-28 12:29:12 -0500

    4

--- src/BinomialTheorem.java
+++ src/BinomialTheorem.java
@@ -9,12 +9,19 @@ public class BinomialTheorem {
         r = numberOfObjectsSelected;
     }
     public BinomialTheorem(){
-        int n = 1;
-        int r = 1;
+         n = 1;
+         r = 1;
     }
 
     //Methods
     public int nFactorial(){
+        int ans=1;
+        for (int nF=n; nF>0; nF--){
+            ans*=nF;
+        }
+        return ans;
+    }
+    public int nFactorial(int n){
         int ans=1;
         for (int nF=n; nF>0; nF--){
             ans*=nF;
@@ -29,8 +36,22 @@ public class BinomialTheorem {
         }
         return ans;
     }
+    public int rFactorial(int r){
+        int ans=1;
+        for (int rF=r; rF>0; rF--){
+            ans*=rF;
+        }
+        return ans;
+    }
 
     public int nMinusRFactorial(){
+        int ans=1;
+        for (int nrF=(n-r); nrF>0; nrF--){
+            ans*=nrF;
+        }
+        return ans;
+    }
+    public int nMinusRFactorial(int n, int r){
         int ans=1;
         for (int nrF=(n-r); nrF>0; nrF--){
             ans*=nrF;
@@ -41,13 +62,27 @@ public class BinomialTheorem {
     public int combination(){
         return permutation()/rFactorial();
     }
+    public int combination(int n, int r){return permutation(n, r)/rFactorial(r);}
 
     public int permutation(){
         return nFactorial()/nMinusRFactorial();
     }
+    public int permutation(int n, int r){return nFactorial(n)/nMinusRFactorial(n,r);}
 
-    public String binomialExpansion() {
-        for (int i = n-1; i>=0; )
+
+
+    public String pascalsTriangle() {
+        int a = n-1;
+        StringBuilder returnString = new StringBuilder();
+        for (int i = 0; i<=n; i++ ){
+            returnString.append(" ".repeat(a + 1));
+            for (int e = 0; e <=i;e++){
+                returnString.append(combination(i, e)).append(" ");
+            }
+            a--;
+            returnString.append("\n");
+        }
+        return returnString.toString();
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,6 @@
 public class Main {
-    public static double Twc(double Ta, double V){
-        return Math.round(35.74+(0.6215*Ta)-(Math.pow(V,0.16)*35.75)+(0.4275*Ta*Math.pow(V, 0.16)));
-    }
     public static void main(String[] args){
-        for (int v = 5; v<=60; v+=5){
-            for(int Ta = 40; Ta>=-45; Ta-=5){
-                System.out.print(Twc(Ta,v)+" ");
-            }
-            System.out.println();
-        }
+        BinomialTheorem test = new BinomialTheorem(10, 6);
+        System.out.println(test.pascalsTriangle());
     }
 }
