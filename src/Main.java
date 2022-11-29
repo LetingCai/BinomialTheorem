aa255e941886d6101647f04263730177e935e412
Leting Cai <letingc@nycstudents.net>
2022-11-29 12:23:12 -0500

    6

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-        BinomialTheorem test = new BinomialTheorem(10, 6);
-        System.out.println();
+        Scanner scan = new Scanner(System.in);
+        int n, r;
+        System.out.print("Welcome to Combination and Permutation Calculator. \nWhat is n (total number of objects)? ");
+        n = scan.nextInt();
+        System.out.print("\nWhat is r (number of objects selected)? ");
+        r = scan.nextInt();
+        BinomialTheorem calc = new BinomialTheorem(n, r);
+        System.out.println(calc);
     }
 }
