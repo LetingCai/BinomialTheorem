4792fa2f3471770cb5abcf9fbfa634fd0294674c
Leting Cai <letingc@nycstudents.net>
2022-12-05 12:33:55 -0500

    7

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,15 @@ public class Main {
         boolean condition = true;
         String str;
         int x;
-        System.out.print("Welcome to Combination and Permutation Calculator!\nEnter nCr for combination (fill in variable n and r)\nEnter nPr for permutation (fill in variable n and r)\nEnter 1 for Pascal's triangle\nEnter 2 for Binomial probability\n Enter -1 to quit");
+        System.out.print("Welcome to Combination and Permutation Calculator!\n\nEnter nCr for combination (fill in variable n and r)\nEnter nPr for permutation (fill in variable n and r)\nEnter 1 for Pascal's triangle\nEnter 2 for Binomial probability\n");
         str = scan.nextLine();
         try{
             x = Integer.parseInt(str);
         } catch(Exception e){
-            if((str.charAt(1) == 'C' || str.charAt(1) == 'D')){
-
+            if(str.charAt(1) == 'C'){
+                System.out.println(BinomialTheorem.combination(Integer.parseInt(str.substring(0,1)),Integer.parseInt(str.substring(2))));
+            } else if(str.charAt(1) == 'P'){
+                System.out.println(BinomialTheorem.permutation(Integer.parseInt(str.substring(0,1)),Integer.parseInt(str.substring(2))));
             }
         }
     }
